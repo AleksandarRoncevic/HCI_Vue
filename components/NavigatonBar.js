964d99a0f06f38419091d2e89a02de47c4ccc33c
Aleksandar Roncevic <dmi.aleksandar.roncevic@student.pmf.uns.ac.rs>
2022-03-21 17:21:24 +0100

    Add navigation bar no styling

---
+++ components/NavigatonBar.js
@@ -0,0 +1,13 @@
+app.component('navigation-bar', {
+    template:
+        `<div class="navigation">
+            <a href="./index.html"><img src="assets/images/logo.png" alt="Logo" class="logo" /></a>
+            <div class="navigation-button">
+                <a href="./index.html">Home</a>
+                <a href="./about.html">About us</a>
+                <a href="./price.html">Rooms & Pricing</a>
+                <a href="./contact.html">Contact Us</a>
+                <a href="./book.html"><button>Book now</button><a/>
+            </div>
+        </div>`
+});

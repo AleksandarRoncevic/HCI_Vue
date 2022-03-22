d90fe61cb6cf08d9cbb3319d28a7b5dc62773ee2
Aleksandar Roncevic <dmi.aleksandar.roncevic@student.pmf.uns.ac.rs>
2022-03-22 17:24:18 +0100

    Add Pricing page and Iterating through pictures

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
 const app = Vue.createApp({
+    data() {
+        return {
+            rooms: [...roomsOffer],
+            roomIndex: 0
+        };
+    },
+    methods: {
+        getRoom(roomIndex) {
+            return this.rooms[roomIndex];
+        },
+        getRoomImage() {
+            return "assets/images/" + this.getRoom(this.roomIndex).count + "room.jpeg";
+        },
+        nextRoom() {
+            this.roomIndex++;
+        },
+        prevRoom() {
+            this.roomIndex--;
+        }
+    }
 });
+
+const roomsOffer = [
+    {
+        count: 10,
+        description: `Desetokrevevtna soba je opremljena sa krevetima na sprat, 
+        zajedničkim kupatilom. S tim u vezi sobe su jeftinije od ostalih u ponudi`,
+        price: 20
+    },
+    {
+        count: 4,
+        description: `Četvorokrevetna soba je idealna za porodicu kojoj je potrebna privatnost 
+        za što bolji užitak.`,
+        price: 30
+    },
+    {
+        count: 2,
+        description: `Perfektna za parove koji obilaze svet da naprave kratat "pit stop"`,
+        price: 40
+    }
+]
